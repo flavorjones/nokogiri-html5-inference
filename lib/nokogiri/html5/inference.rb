1b2ecde5b26f738211127ba98f067f1d4bbe844d
Mike Dalessio <mike.dalessio@gmail.com>
2024-04-24 10:52:01 -0400

    basic functionality

--- lib/nokogiri/html5/inference.rb
+++ lib/nokogiri/html5/inference.rb
@@ -1,12 +1,49 @@
 # frozen_string_literal: true
 
 require_relative "inference/version"
+require "nokogiri"
 
-module Nokogiri
-  module Html5
-    module Inference
-      class Error < StandardError; end
-      # Your code goes here...
+if defined?(Nokogiri::HTML5::Inference)
+  # keep option open of merging into Nokogiri some day
+  warn "NOTE: nokogiri-html5-inference is already loaded. Check for pinned issues at https://github.com/flavorjones/nokogiri-html5-inference/issues for more information."
+else
+  module Nokogiri
+    module HTML5
+      module Inference
+        module Tags
+          TABLE = %w[thead tbody tfoot tr td th col colgroup caption].freeze
+          HTML = %w[head body].freeze
+        end
+
+        module Regexp
+          DOCUMENT = /\A\s*(<!doctype\s+html\b|<html\b)/i
+          TABLE = /\A\s*<(#{Tags::TABLE.join("|")})\b/i
+          HTML = /\A\s*<(#{Tags::HTML.join("|")})\b/i
+        end
+
+        class << self
+          def context(input)
+            peek = input[0, 100]
+
+            case peek
+            when Regexp::DOCUMENT then nil
+            when Regexp::TABLE then "table"
+            when Regexp::HTML then "html"
+            else
+              "body"
+            end
+          end
+
+          def parse(input)
+            context = Nokogiri::HTML5::Inference.context(input)
+            if context.nil?
+              Nokogiri::HTML5::Document.parse(input)
+            else
+              Nokogiri::HTML5::DocumentFragment.new(Nokogiri::HTML5::Document.new, input, context)
+            end
+          end
+        end
+      end
     end
   end
 end
--- test/nokogiri/html5/test_inference.rb
+++ test/nokogiri/html5/test_inference.rb
@@ -2,12 +2,99 @@
 
 require "test_helper"
 
-class Nokogiri::Html5::TestInference < Minitest::Test
-  def test_that_it_has_a_version_number
-    refute_nil ::Nokogiri::Html5::Inference::VERSION
+describe Nokogiri::HTML5::Inference do
+  fragment_actions = {
+    "body" => [
+      "<div>hello</div>",
+      "<div class=\"big\">hello</div>",
+      "<li>hello</li>",
+      "<dl><dd>hello</dd><dt>world</dt></dl>",
+      "<dd>hello</dd><dt>world</dt>",
+      "just some text"
+    ],
+    "table" => [
+      "<thead><tr><td>hello</td></tr></thead>",
+      "<tbody><tr><td>hello</td></tr></tbody>",
+      "<tfoot><tr><td>hello</td></tr></tfoot>",
+      "<tr><th>hello</th></tr>",
+      "<tr><td>hello</td></tr>",
+      "<th>hello</th>",
+      "<td>hello</td>",
+      "<colgroup><col class=\"hello\"></colgroup>",
+      "<col class=\"hello\">",
+      "<caption>hello</caption>"
+    ],
+    "html" => [
+      "<body><div>hello</div></body>",
+      "<head><meta charset=\"UTF-8\"><title>hello</title></head>"
+    ]
+  }
+
+  describe ".context" do
+    describe "passed a Document with doctype" do
+      it "returns nil" do
+        assert_nil(Nokogiri::HTML5::Inference.context("<!doctype html><html><head></head><body></body></html>"))
+        assert_nil(Nokogiri::HTML5::Inference.context(" <!doctype   html><html><head></head><body></body></html>"))
+        assert_nil(Nokogiri::HTML5::Inference.context("<!DOCTYPE HTML><HTML><HEAD></HEAD><BODY></BODY></HTML>"))
+      end
+    end
+
+    describe "passed a Document without doctype" do
+      it "returns nil" do
+        assert_nil(Nokogiri::HTML5::Inference.context("<html><head></head><body></body></html>"))
+        assert_nil(Nokogiri::HTML5::Inference.context(" <html lang='en'><head></head><body></body></html>"))
+        assert_nil(Nokogiri::HTML5::Inference.context("<HTML><HEAD></HEAD><BODY></BODY></HTML>"))
+      end
+    end
+
+    fragment_actions.each do |context, fragments|
+      describe "passed a fragment requiring 'in #{context}' insertion mode" do
+        it "returns '#{context}'" do
+          fragments.each do |fragment|
+            actual = Nokogiri::HTML5::Inference.context(fragment)
+            assert_equal(context, actual, "   Given: #{fragment.inspect}")
+          end
+        end
+      end
+    end
   end
 
-  def test_it_does_something_useful
-    assert false
+  describe ".parse" do
+    describe "passed a Document with doctype" do
+      it "returns a Document" do
+        assert_equal(
+          "<!DOCTYPE html><html><head></head><body></body></html>",
+          Nokogiri::HTML5::Inference.parse("<!doctype html><html><head></head><body></body></html>").to_html
+        )
+        assert_equal(
+          "<!DOCTYPE html><html><head></head><body></body></html>",
+          Nokogiri::HTML5::Inference.parse("<!DOCTYPE HTML><HTML><HEAD></HEAD><BODY></BODY></HTML>").to_html
+        )
+      end
+    end
+
+    describe "passed a Document without doctype" do
+      it "returns a Document" do
+        assert_equal(
+          "<html><head></head><body></body></html>",
+          Nokogiri::HTML5::Inference.parse("<html><head></head><body></body></html>").to_html
+        )
+        assert_equal(
+          "<html><head></head><body></body></html>",
+          Nokogiri::HTML5::Inference.parse("<HTML><HEAD></HEAD><BODY></BODY></HTML>").to_html
+        )
+      end
+    end
+
+    fragment_actions.each do |context, fragments|
+      describe "passed a fragment requiring 'in #{context}' insertion mode" do
+        fragments.each do |fragment|
+          it "parses '#{fragment}' correctly" do
+            actual = Nokogiri::HTML5::Inference.parse(fragment).to_html
+            assert_equal(fragment, actual)
+          end
+        end
+      end
+    end
   end
 end
